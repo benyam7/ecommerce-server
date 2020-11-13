0e88ab3fb3b7b00fa2792065eed1748b000e43c3
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2020-11-13 20:16:49 +0300

    feat: get current user

--- index.js
+++ index.js
@@ -8,15 +8,40 @@ const models = require('./src/models');
 
 const secret = process.env.SECRET;
 const PORT = process.env.PORT || 5000;
+
+// get current user
+const getCurrentUser = async (req) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split('Bearer ')[1];
+
+    if (token) {
+      try {
+        const user = await jwt.verify(token, secret);
+        return user;
+      } catch (e) {
+        return null;
+      }
+    }
+    // if token is not in right format
+    throw new Error("Token is not in a format 'Bearer token' ");
+  }
+};
+// setup server
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
-    models,
-    secret,
+  context: async ({ req }) => {
+    if (req) {
+      const currentUser = await getCurrentUser(req);
+
+      return { models, currentUser, secret };
+    }
   },
 });
 
+// connect to db
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_CONN, {
